50f5efde85b0a43368bc4887dba5d789cc4f4003
Ha-no <gksdn1216@naver.com>
2023-02-27 10:38:25 +0900

    [UPDATE] BestTopic Skeleton

--- src/components/BestTopic/BestTopicPresenter.jsx
+++ src/components/BestTopic/BestTopicPresenter.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
+import '../../css/besttopic.css'
 
 const BestTopicPresenter = ( props ) => {
 
-    const { testfunction } = props;
+    const { testfunction, testchange } = props;
 
     return (
         <div>
             {/* UI 구성 */}
-            <p>검색</p>
-            <p>베스트 게시글</p>
-            <button onClick={ testfunction } >wow</button>
+            <p>검색 추가</p>
+            <p>베스트 게시글 추가</p>
+            <div className="test">
+                <button onClick={ testfunction } >wow</button>
+                <input type="text" name="title" onChange={testchange} />
+            </div>
         </div>
     );
 }
