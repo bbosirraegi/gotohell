977c0f931068f8e0898ce97bf5460986d91f8a2e
bamsaengki <bamsaengki@gmail.com>
2023-02-28 14:23:06 +0900

    [UPDATE]BestTopic add

--- src/components/BestTopic/BestTopicPresenter.jsx
+++ src/components/BestTopic/BestTopicPresenter.jsx
@@ -1,7 +1,12 @@
 import React, { useRef, useState } from "react";
 import "../../css/besttopic.css";
 import attendimg from "../../img/attend.png";
+import BestTopicImg from "../../img/SingCong.png";
 import SearchIcon from "@mui/icons-material/Search";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import GitHubIcon from "@mui/icons-material/GitHub";
 import { Link, useNavigate } from "react-router-dom";
 
 const BestTopicPresenter = (props) => {
@@ -48,17 +53,48 @@ const BestTopicPresenter = (props) => {
           />
         </div>
         <div className="Best_Topic_Top">
-          <div>
-            <span className="Best_Topic_Text">베스트 게시글</span>
-          </div>
+          <span className="Best_Topic_Text">베스트 게시글</span>
           <a href="/#" className="Best_Topic_Link">
             전체 보기 {" >"}
           </a>
         </div>
-        <div className="Best_Topic_1">
-          sdfsdf
-          <hr className="Under_Line" />
+
+        <div className="Best_Topic">
+          <div className="Best_Topic_Frame">
+            <div className="Best_Topic_Title">오늘 저녁은 뭐먹지?</div>
+            <div className="Best_Topic_PostText">
+              몰?루 몰?루 몰?루 몰?루 몰?루 몰?루 몰?루 몰?루 몰?루 몰?루 몰?루
+              몰?루 몰?루 몰?루 몰?루 몰?루 몰?루 몰?루 몰?루 몰?루 몰?루 몰?루
+              몰?루 몰?루 몰?루 몰?루 몰?루 몰?루 몰?루 몰?루
+            </div>
+          </div>
+          <img src={BestTopicImg} className="Best_Topic_Img" />
         </div>
+
+        <div className="Best_Topic">
+          <div className="Best_Topic_Frame">
+            <div className="Best_Topic_Title">오늘 저녁은 뭐먹지?</div>
+            <div className="Best_Topic_PostText">
+              몰?루 몰?루 몰?루 몰?루 몰?루 몰?루 몰?루 몰?루 몰?루 몰?루 몰?루
+              몰?루 몰?루 몰?루 몰?루 몰?루 몰?루 몰?루 몰?루 몰?루 몰?루 몰?루
+              몰?루 몰?루 몰?루 몰?루 몰?루 몰?루 몰?루 몰?루
+            </div>
+          </div>
+          <img src={BestTopicImg} className="Best_Topic_Img" />
+        </div>
+
+        <div className="Best_Topic_last">
+          <div className="Best_Topic_Frame">
+            <div className="Best_Topic_Title">오늘 저녁은 뭐먹지?</div>
+            <div className="Best_Topic_PostText">
+              몰?루 몰?루 몰?루 몰?루 몰?루 몰?루 몰?루 몰?루 몰?루 몰?루 몰?루
+              몰?루 몰?루 몰?루 몰?루 몰?루 몰?루 몰?루 몰?루 몰?루 몰?루 몰?루
+              몰?루 몰?루 몰?루 몰?루 몰?루 몰?루 몰?루 몰?루
+            </div>
+          </div>
+          <img src={BestTopicImg} className="Best_Topic_Img" />
+        </div>
+
         {/* <div className="test">
           <button onClick={testfunction}>wow</button>
           <input type="text" name="title" onChange={testchange} />
@@ -76,8 +112,8 @@ const BestTopicPresenter = (props) => {
           <span className="CopyRight">
             <Link to="/">서비스 정보 수신 동의 </Link>
           </span>
-          <div>
-            <details>
+          <div className="GTH_Details">
+            <details style={{}}>
               <summary>GTH</summary>
               <span>
                 <br />
@@ -85,15 +121,31 @@ const BestTopicPresenter = (props) => {
                 <br />
                 사업자등록번호 : 111 - 11 - 11111
                 <br />
-                이름 : 알빠야? <br />
+                이름 : 알빠야?
+                <br />
                 직책 : 개인정보
                 <br />
-                보호책임자 메일 : <br />
+                보호책임자 메일 : 몰?루
+                <br />
                 주소 : 부산광역시 금정구 부산대학로63번길 2
                 <br />
                 통신판매업번호 : 없음
               </span>
             </details>
+          </div>
+          <div className="SNS">
+            <Link to="/" className="SNS_ICONS">
+              <InstagramIcon />
+            </Link>
+            <Link to="/" className="SNS_ICONS">
+              <FacebookIcon />
+            </Link>
+            <Link to="/" className="SNS_ICONS">
+              <YouTubeIcon />
+            </Link>
+            <Link to="/" className="SNS_ICONS">
+              <GitHubIcon />
+            </Link>
           </div>
         </div>
       </div>
